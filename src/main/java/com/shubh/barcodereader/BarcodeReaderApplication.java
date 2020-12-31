9d9b9813fe7fd6a682c20091164a92e75dbba9b7
shubh-shah <shubhshah09@gmail.com>
2021-01-01 01:22:55 +0530

    QR code generation and pairing done

--- src/main/java/com/shubh/barcodereader/BarcodeReaderApplication.java
+++ src/main/java/com/shubh/barcodereader/BarcodeReaderApplication.java
@@ -1,26 +1,49 @@
 package com.shubh.barcodereader;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
-import java.awt.Robot;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.security.SecureRandom;
+import java.util.Enumeration;
+import javax.imageio.ImageIO;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import java.awt.Desktop;
+import java.awt.Robot;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class BarcodeReaderApplication {
 	
 	private Robot robot;
 	private String password;
+	private final int password_length = 13;
 	private static final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	private static SecureRandom rnd = new SecureRandom();
+	private int pairedDevices;
 
 	public BarcodeReaderApplication(){
-		password = randomStringGenerator(13);
-		System.out.println("Password : "+password);
+		StringBuilder sb = new StringBuilder(password_length);
+		for(int i = 0; i < password_length; i++){
+			sb.append(AB.charAt(rnd.nextInt(AB.length())));
+		}
+		password = sb.toString();
 		try {
 			robot = new Robot(); 
 		}
@@ -29,15 +52,6 @@ public class BarcodeReaderApplication {
 			e.printStackTrace();
 			System.exit(0);
 		}
-
-	}
-
-	private String randomStringGenerator(int len){
-		StringBuilder sb = new StringBuilder(len);
-		for(int i = 0; i < len; i++){
-			sb.append(AB.charAt(rnd.nextInt(AB.length())));
-		}
-		return sb.toString();
 	}
 
 	public static void main(String[] args) {
@@ -45,24 +59,42 @@ public class BarcodeReaderApplication {
 		builder.headless(false).run(args);
 	}
 
-	@GetMapping("/")
-	public String docs() {
-		return String.format("Send post request to /post-barcode with json object containg two strings with labels : content and password");
+	@GetMapping(value="/", produces="text/html")
+	public String homepage(Model model) {
+		model.addAttribute("message", pairedDevices+" Paired Devices");
+		return "index";
 	}
 
+	@CrossOrigin
+	@ResponseBody
+	@GetMapping(value="/pair", produces = "application/json")
+	public String pair() throws IOException{
+		pairedDevices++;
+		Desktop.getDesktop().open(new File("./web/index.html"));
+		return "{\"message\":\"Success\"}";
+	}
+
+	@ResponseBody
 	@PostMapping(value = "/post-barcode", consumes = "application/json")
-	public String post_barcode(@RequestBody ReadString request) {
+	public String postBarcode(@RequestBody ReadString request) {
+		String req_str = request.getContent().toUpperCase();
+		//Check if input is valid
 		if(!password.equals(request.getPassword())){
 			System.out.println("Got request with mismatched password");
-			return "Failure";
+			return "Incorrect Password";
 		}
-		String req_str = request.getContent().toUpperCase();//
 		for (int i = 0; i < req_str.length(); i++){
 			if((req_str.charAt(i)<'0' || req_str.charAt(i)>'9') && (req_str.charAt(i)<'A' || req_str.charAt(i)>'Z') && req_str.charAt(i)!=' '){
 				System.out.println("Got request with non alphanumeric characters : "+req_str);
-				return "Failure";
+				return "Input Invalid";
 			}
 		}
+		//For password check
+		if(req_str.equals("")){
+			System.out.println("Got a ping");
+			return "Success";
+		}
+		//To recieve keystrokes
 		for (int i = 0; i < req_str.length(); i++){
 			robot.keyPress(req_str.charAt(i)); 
 			robot.keyRelease(req_str.charAt(i)); 	
@@ -73,4 +105,68 @@ public class BarcodeReaderApplication {
 		return "Success";
 	}
 
+	@CrossOrigin
+	@ResponseBody
+	@GetMapping(value = "/generate-qr", produces = "application/json")
+	public PairOutput generateQR(){
+		PairOutput out = new PairOutput();
+		out.path="";
+		try{
+			//Construct string
+			String QRString = "{\"url\":[";
+			Enumeration<NetworkInterface> listNI= NetworkInterface.getNetworkInterfaces();
+			while(listNI.hasMoreElements()){
+				Enumeration<InetAddress> listIP = listNI.nextElement().getInetAddresses();
+				while(listIP.hasMoreElements()){
+					QRString+="\""+((InetAddress)listIP.nextElement()).getHostAddress()+"\",";
+				}
+			}
+			if(QRString.endsWith(",")){
+				QRString = QRString.substring(0, QRString.length()-1);
+			}
+			QRString+="],\"password\":\""+password+"\"}";
+			
+			//Construct QR Code
+			QRCodeWriter barcodeWriter = new QRCodeWriter();
+			BitMatrix bitMatrix = barcodeWriter.encode(QRString, BarcodeFormat.QR_CODE, 200, 200);
+			
+			//Write OR to file
+			File outputfile = new File("QRImage.png");
+			out.path = outputfile.getAbsolutePath();
+			BufferedImage img = MatrixToImageWriter.toBufferedImage(bitMatrix);
+			out.height=img.getHeight();
+			out.width=img.getWidth();
+			ImageIO.write(img, "png", outputfile);	
+			out.status=0;
+			out.error="NoError";			
+		}
+		catch(SocketException e){
+			System.out.println("Couldn't find IPs");
+			e.printStackTrace();
+			out.status=1;
+			out.error=e.getClass().getSimpleName();
+		}
+		catch(WriterException e){
+			System.out.println("Couldn't generate QR");
+			e.printStackTrace();
+			out.status=1;
+			out.error=e.getClass().getSimpleName();
+		}
+		catch(IOException e){
+			System.out.println("Couldn't write QR to file");
+			e.printStackTrace();
+			out.status=1;
+			out.error=e.getClass().getSimpleName();
+		}
+		return out;
+	}
+
+}
+
+class PairOutput{
+	public int status;
+	public String error;
+	public String path;
+	public int height;
+	public int width;
 }
